9a786897268f61b601acaec91bb6cd69b8196cf8
barthez1993 <barthez1993@gmail.com>
2014-05-30 11:29:34 +0200

    poczatkowa wersja

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <SFML/Graphics.hpp>
+#include <cmath>
+using namespace sf;
+int main()
+{
+	RenderWindow okno(VideoMode(320, 240), "Kurs SFML 2.0 - http://cpp0x.pl");
+	Clock stoper;
+	while (okno.isOpen())
+	{
+		Event event;
+		while (okno.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				okno.close();
+
+		} //while
+		okno.clear();
+
+		CircleShape ksztalt(std::sin(stoper.getElapsedTime().asSeconds()) * okno.getSize().y / 8 + okno.getSize().y / 4);
+		ksztalt.setOrigin(Vector2f(ksztalt.getRadius(), ksztalt.getRadius()));
+		ksztalt.setPosition(okno.getSize().x / 2.0f, okno.getSize().y / 2.0f);
+		ksztalt.setFillColor(Color::Yellow);
+		okno.draw(ksztalt);
+
+		okno.display();
+	} //while
+	return 0;
+}
